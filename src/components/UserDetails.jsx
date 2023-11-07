b798e1d7140157dc1fc633a1d269ad9a6383474e
EHAB3x <ehabmah6969@gmail.com>
2023-11-08 00:35:02 +0200

    restructure useParams()

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
 import { useParams } from 'react-router-dom'
+import React from 'react'
 
 export const UserDetails = () => {
-    const id = useParams()
-    console.log(id.userId);
+    // const {userId} = useParams();
+    const params = useParams();
+    const userId = params.userId;
   return (
     <div>
-        {`Details About User # ${id.userId}`}
+        {`Details About User # ${userId}`}
     </div>
   )
 }
